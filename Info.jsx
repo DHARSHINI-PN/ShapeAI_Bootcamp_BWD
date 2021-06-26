4ed9c7e64701543e8d8b68adb37cddd5ee099cb0
DHARSHINI PN <85385444+DHARSHINI-PN@users.noreply.github.com>
2021-06-26 18:27:26 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Admissions open!!!</h1>
+      <p>Courses offered: CSE, IT, ECE, EEE, Mech, Civil....</p>
+    </div>
+  );
+}
+
+export default Info;
